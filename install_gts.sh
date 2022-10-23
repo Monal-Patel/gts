a596bf795a506de0a6fb983e9582cc5d0cf75ba7
Monal-Patel <monal-@hotmail.com>
2022-10-23 18:04:09 +0100

    update install_gtswq

--- install_gts.sh
+++ install_gts.sh
@@ -2,4 +2,16 @@
 ./autogen.sh
 ./configure --prefix=$PWD/install
 make install 
+
 # need to add GTS_HOME/install/lib to $PATH and $LD_LIBRARY_PATH
+file=$HOME/.bashrc
+if [ -f "$file" ] 
+then
+	echo "export GTS_HOME=$PWD" >> $file
+	echo 'export LD_LIBRARY_PATH=$GTS_HOME/install/lib:$LD_LIBRARY_PATH' >> $file
+	echo 'export PATH=$GTS_HOME/install/lib:$PATH' >> $file
+	source ~/.bashrc
+else
+	echo "$file not found"
+	exit 2
+fi
